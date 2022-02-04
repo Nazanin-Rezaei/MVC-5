c5b51eb662a594023dd64b8eea58709729fa11a0
nazaninrezaei88@gmail.com <nazaninrezaei88@gmail.com>
2022-02-04 08:18:52 -0500

    Restaurant App

---
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Data.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [Display(Name = "Type of food")]
+        public CuisineType Cuisine { get; set; }
+    }
+}
---
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using OdeToFood.Data.Services;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer(HttpConfiguration httpConfigration)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterApiControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<SqlRestaurantData>()
+                .As<IRestaurantData>()
+                .InstancePerRequest();
+            builder.RegisterType<OdeToFoodDbContext>().InstancePerRequest();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            httpConfigration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
